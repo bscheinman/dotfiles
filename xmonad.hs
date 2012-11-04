01aa8015bae373856bdeb00f80bf0b8fbd70c78c
Brendon Scheinman <bscheinman@gmail.com>
2012-11-04 10:01:24 -0600

    added basic configs from laptop

---
+++ xmonad.hs
@@ -0,0 +1,32 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Grid
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/brendon/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = myLayoutHook
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        , modMask = mod4Mask
+        , normalBorderColor = "#000000"
+        , focusedBorderColor = "#000000"
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_a), spawn "chromium-browser")
+        , ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+        , ((mod4Mask .|. shiftMask, xK_h), spawn "sudo /usr/sbin/pm-hibernate")
+        , ((mod4Mask .|. shiftMask, xK_s), spawn "sudo /usr/sbin/pm-suspend")
+        , ((mod4Mask .|. shiftMask, xK_z), sendMessage $ ToggleStrut R)
+        ]
+
+myLayoutHook = avoidStrutsOn [U] (
+        Tall 1 (3/100) (1/2)
+    ||| Full
+    ||| Grid)
