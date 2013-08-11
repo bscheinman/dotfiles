02fc3d7d122ff0a6c98e86626e1b91b3aa1e05cd
Brendon Scheinman <bscheinman@gmail.com>
2013-08-11 09:36:48 -0500

    added mute/unmute controls

--- xmonad.hs
+++ xmonad.hs
@@ -18,15 +18,17 @@ main = do
             }
         , modMask = mod4Mask
         , normalBorderColor = "#000000"
-        , focusedBorderColor = "#000000"
+        , focusedBorderColor = "#0000FF"
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_a), spawn "chromium-browser")
         , ((mod4Mask .|. shiftMask, xK_z), spawn "chromium-browser --incognito")
         , ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-        , ((mod4Mask .|. shiftMask, xK_h), spawn "sudo /usr/sbin/pm-hibernate")
-        , ((mod4Mask .|. shiftMask, xK_s), spawn "sudo /usr/sbin/pm-suspend")
+        , ((mod4Mask .|. shiftMask, xK_h), spawn "sudo pm-hibernate")
+        , ((mod4Mask .|. shiftMask, xK_s), spawn "sudo pm-suspend")
         , ((mod4Mask , xK_F1), spawn "amixer sset Master 5-")
         , ((mod4Mask , xK_F2), spawn "amixer sset Master 5+")
+	, ((mod4Mask , xK_F3), spawn "pactl set-sink-mute 0 1")
+	, ((mod4Mask , xK_F4), spawn "pactl set-sink-mute 0 0")
         ]
 
 tallLayout = Tall 1 (3/100) (1/2)
